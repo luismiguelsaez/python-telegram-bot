ca0d49586ce09a75cfd44c27ce63b3945eaff9e6
h4rdl1nk <imthedanger@protonmail.com>
2019-01-14 14:30:55 +0100

    Added run_output function

--- shell/process.py
+++ shell/process.py
@@ -27,3 +27,21 @@ def run(command):
         return_code = 1
 
     return return_code
+
+def run_output(command):
+
+    exec_command = command.split(' ')
+
+    return_code = -1
+
+    result = {}
+
+    try:
+        p = subprocess.run(args=exec_command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
+        result['code'] = p.returncode
+        result['output'] = p.stdout
+    except Exception as err:
+        result['code'] = return_code
+        result['output'] = err
+
+    return result
